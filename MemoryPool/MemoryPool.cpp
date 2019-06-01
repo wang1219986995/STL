043635183cbb3a40cfe702a537687a13838aa22a
何泽文 <1246485286@qq.com>
2019-06-01 11:50:50 +0800

    添加项目文件。

---
+++ MemoryPool/MemoryPool.cpp
@@ -0,0 +1,80 @@
+﻿#include "MemoryPool.h"
+
+namespace hzw
+{
+	std::mutex MemoryPool::_mutexs[MemoryPool::ChainLength];
+	MemoryPool::Node *volatile MemoryPool::_pool[MemoryPool::ChainLength]{ nullptr };
+	std::mutex MemoryPool::_freeMutex;
+	char *volatile MemoryPool::_freeBegin = nullptr;
+	char *volatile MemoryPool::_freeEnd = nullptr;
+	volatile size_t MemoryPool::_count = 0;
+
+	MemoryPool::Node * MemoryPool::splitFreePool(size_t size)
+	{
+		Node *result{ reinterpret_cast<Node *>(_freeBegin) };
+		size_t splitSize{ (_freeEnd - _freeBegin) / size };
+		splitSize = splitSize < MaxSplitSize ? splitSize : MaxSplitSize;
+		//切割
+		char *p{ _freeBegin };
+		for (size_t i{ 1 }; i < splitSize; ++i, p += size)
+			reinterpret_cast<Node *>(p)->next = reinterpret_cast<Node *>(p + size);
+		reinterpret_cast<Node *>(p)->next = nullptr;
+		//重置战备池
+		_freeBegin = p + size;
+		return result;
+	}
+
+	void MemoryPool::fillChain(size_t size)
+	{
+		size_t lastSize{ static_cast<size_t>(_freeEnd - _freeBegin) };
+		//生成新链
+		if (lastSize < size)//战备池无法满足一个需求
+		{
+			if (lastSize)		addToChain(reinterpret_cast<Node *>(_freeBegin), lastSize);//处理剩余
+			//填充战备池
+			size_t askSize{ size * AskPerSize + alignSize(_count / 4) };//增长量		
+			if (_freeBegin = reinterpret_cast<char *>(std::malloc(askSize)))//向系统申请内存成功
+			{
+				_freeEnd = _freeBegin + askSize;
+				_count += askSize;
+			}
+			else//向系统申请失败，分割大的内存链
+			{
+				size_t targetIndex{ searchIndex(size) + 1 };
+				for (; targetIndex < ChainLength; ++targetIndex)
+				{
+					_mutexs[targetIndex].lock();
+					if (_pool[targetIndex])	break;
+					_mutexs[targetIndex].unlock();
+				}
+				if (targetIndex >= ChainLength)  throw std::bad_alloc{};//找不到可分割的大内存链
+				else
+				{
+					_freeBegin = reinterpret_cast<char *>(removeFromChain(_pool[targetIndex]));
+					_mutexs[targetIndex].unlock();
+					_freeEnd = _freeBegin + (targetIndex + 1) * ChainPerSize;
+				}
+			}
+		}
+		_pool[searchIndex(size)] = splitFreePool(size);//加入新链
+	}
+
+	void * MemoryPool::_allocate(size_t size)
+	{
+		size_t index{ searchIndex(size) };
+		std::lock_guard<std::mutex> poolMutex{ _mutexs[index] };
+		if (!_pool[index])//对应内存链为空则填充
+		{
+			if (!_freeMutex.try_lock())//战备池上锁失败则放弃对应内存链锁
+			{
+				_mutexs[index].unlock();
+				std::lock(_freeMutex, _mutexs[index]);
+				if (_pool[index])		goto unFill;//双重检测
+			}
+			fillChain(size);
+		unFill:
+			_freeMutex.unlock();
+		}
+		return removeFromChain(_pool[index]);
+	}
+}
---
+++ MemoryPool/MemoryPool.h
@@ -0,0 +1,178 @@
+﻿#ifndef MEMORYPOOL_H
+#define MEMORYPOOL_H
+#include <exception>
+#include <mutex>
+
+namespace hzw
+{
+	/*
+	线程安全类
+	测试量级：千万
+	对比基准：::operator new
+	内存：少20%
+	速度：
+		单线程：快10%
+		多线程：多个线程请求相同大小速度不如operator new（请求内存大小相同的线程越多速度越慢）, 请求不同大小速度快10%
+	总结：请求内存越小，各线程请求内存大小各不相同，时空效率越优
+	*/
+	class MemoryPool
+	{
+	private:
+		enum {
+			ChainLength = 16, ChainPerSize = 8, Log2ChainPerSize = 3,
+			MaxSize = ChainLength * ChainPerSize, AskPerSize = 40, MaxSplitSize = 20
+		};
+		//内存池链的个数  //内存池粒度 //log2ChainPerSize的指 //管理的最大内存 //每次申请内存个数 //最大切割个数
+
+		struct Node
+		{
+			Node *next;
+		};
+		static std::mutex _mutexs[ChainLength];//内存池互斥量
+		static Node *volatile _pool[ChainLength];//内存池
+		static std::mutex _freeMutex;//战备池互斥量
+		static char *volatile _freeBegin, *volatile _freeEnd;//战备池指针
+		static volatile size_t _count;//内存池管理内存总量
+
+	private:
+		//功能：从内存池分配内存
+		//输入：对齐后内存需求大小
+		//输出：分配内存块的指针
+		static void *_allocate(size_t size);
+
+		//功能：切割战备池后调整战备池大小
+		//输入：对齐后内存需求大小
+		//输出：切割完成后的内存链
+		static Node *splitFreePool(size_t size);
+
+		//功能：填充内存链（填充内存来源：战备池，malloc，更大的内存链）
+		//输入：对齐后内存需求大小
+		static void fillChain(size_t size);
+
+		//功能：查找内存需求对应的内存链索引
+		//输入：内存需求大小
+		//输出：内存链索引
+		constexpr static size_t searchIndex(size_t size)
+		{
+			return (size + ChainPerSize - 1 >> Log2ChainPerSize) - 1;
+		}
+
+		//功能：内存需求与ChainPerSize对齐
+		//输入：内存需求大小
+		//输出：对齐后内存需求大小
+		constexpr static size_t alignSize(size_t size)
+		{
+			return (size + ChainPerSize - 1 & ~(ChainPerSize - 1));
+		}
+
+		//功能：向对应内存链添加新块
+		//输入：新块指针，内存需求大小
+		static void addToChain(Node *p, size_t size)
+		{
+			size_t index{ searchIndex(size) };
+			_mutexs[index].lock();
+			Node *volatile &chainHead{ _pool[index] };
+			p->next = chainHead;
+			chainHead = p;
+			_mutexs[index].unlock();
+		}
+
+		//功能：移除对应内存链头部的块
+		//输入：对应内存链指针引用
+		//输出：移除的块指针
+		static void *removeFromChain(Node * volatile&chainHead)
+		{
+			void *result{ chainHead };
+			chainHead = chainHead->next;
+			return result;
+		}
+
+		//功能：归还内存池内存
+		//输入：归还内存的指针，对齐后内存需求大小
+		static void _deallocate(Node *oldP, size_t size)
+		{
+			addToChain(oldP, size);
+		}
+
+	public:
+		MemoryPool() = delete;
+
+		//功能：分配内存资源
+		//输入：内存需求的大小
+		//输出：分配后的内存指针
+		static void *allocate(size_t size)
+		{
+			return size > MaxSize ? ::operator new(size) : _allocate(alignSize(size));
+		}
+
+		//功能：释放内存资源
+		//输入：释放内存的指针，释放内存的大小
+		static void deallocate(void *oldP, size_t size)
+		{
+			size > MaxSize ? ::operator delete(oldP) : _deallocate(reinterpret_cast<Node *>(oldP), alignSize(size));
+		}
+	};
+
+	/*
+	类使用MemoryPool直接继承此类
+	用基类指针delete谨慎使用，size值不正确，内存回收异常
+	*/
+	class UseMemoryPool
+	{
+	public:
+		void *operator new(size_t size)
+		{
+			return MemoryPool::allocate(size);
+		}
+
+		void operator delete(void *oldP, size_t size)
+		{
+			MemoryPool::deallocate(oldP, size);
+		}
+	};
+
+	/*
+	容器分配器内部为MemoryPool
+	*/
+	template<typename T>
+	class Allocator
+	{
+	public:
+		using value_type = T;
+		using pointer = T * ;
+		using const_pointer = const T *;
+		using reference = T & ;
+		using const_reference = const T&;
+		using size_type = size_t;
+		using difference_type = ptrdiff_t;
+
+		value_type *allocate(std::size_t num)
+		{
+			return reinterpret_cast<value_type *>(MemoryPool::allocate(num * sizeof(value_type)));
+		}
+
+		void deallocate(value_type *p, std::size_t num)
+		{
+			MemoryPool::deallocate(p, num * sizeof(value_type));
+		}
+
+		template<typename U>
+		bool operator ==(const Allocator<U>& lh)
+		{
+			return true;
+		}
+
+		template<typename U>
+		bool operator !=(const Allocator<U>& lh)
+		{
+			return false;
+		}
+
+		template<typename U>
+		operator Allocator<U>()const
+		{
+			return Allocator<U>{};
+		}
+	};
+}
+#endif //MEMORYPOOL_H
